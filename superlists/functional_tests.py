5d975a0eb4dc6f29e62b98905f4c5418651069be
lpatmo <linda.peng@alumni.duke.edu>
2017-10-16 20:20:09 -0700

    added new tests and rewrote view

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -38,17 +38,21 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)  
 
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')  
-        self.assertTrue(
-        any(row.text == '1: Buy peacock feathers' for row in rows),
-        	"New to-do item did not appear in table"
-    	)
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+        '2: Use peacock feathers to make a fly',
+         [row.text for row in rows]
+        )
+
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
         # methodical)
         self.fail('Finish the test!')
         # The page updates again, and now shows both items on her list
+
+
 
 if __name__ == '__main__':  
     unittest.main(warnings='ignore')
